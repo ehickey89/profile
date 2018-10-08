7f972b08cbae2971d8e2fda9c29304367d009075
Emily Hickey <38540984+ehickey89@users.noreply.github.com>
2018-10-08 16:15:01 -0400

    updated nav

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -72,3 +73,79 @@ app.post('/send', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server started...'));
+=======
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/send', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'gator3235.hostgator.com', 
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: 'info@emilyrhickey.com', // generated ethereal user
+            pass: 'r8o1s8e9'  // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Emily Hickey" <info@emilyrhickey.com>', // sender address
+        to: 'emilyrhickey89@gmail.com', // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', { msg: 'Email has been sent' });
+    });
+});
+
+app.listen(3000, () => console.log('Server started...'));
+>>>>>>> 5847917ef23e94c85341903bd5e8d3746c9a0653
--- public/scroll.js
+++ public/scroll.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 //navbar color change on scroll
 $(function () {
@@ -15,3 +16,22 @@ $(function () {
 });
 
 
+=======
+
+//navbar color change on scroll
+$(function () {
+  var header = $(".navbar");
+
+  $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      header.addClass("scrolled");
+    } else {
+      header.removeClass("scrolled");
+    }
+  });
+
+});
+
+
+>>>>>>> 5847917ef23e94c85341903bd5e8d3746c9a0653
